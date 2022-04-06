472a9d26d202bd4c034396868fbf945f3ac14b07
rane <pessi.raunio@tuni.fi>
2022-04-06 15:28:46 +0300

    Include client files, .gitignore and database

---
+++ db/init_db.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS public.users
+(
+    id character varying(36) COLLATE pg_catalog."default" NOT NULL,
+    name character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(60) COLLATE pg_catalog."default" NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.plans
+(
+    id character varying(36) COLLATE pg_catalog."default" NOT NULL,
+    title character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    description character varying(300) COLLATE pg_catalog."default" NOT NULL,
+    category character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    creator character varying(36) COLLATE pg_catalog."default" NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+);
